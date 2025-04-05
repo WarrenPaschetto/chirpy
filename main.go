1082d0b250e266a4ddfb62d905a8a1a3b3010bbb
Warren Paschetto <wdpaschetto@gmail.com>
2025-04-05 17:36:50 -0400

    added method to retrieve chirp by ID

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
 	srv := &http.Server{
