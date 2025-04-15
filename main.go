78cf9891ebfe12f9bb28b9c899b9fbe639d06b54
Warren Paschetto <wdpaschetto@gmail.com>
2025-04-15 09:26:31 -0400

    added authorization

--- main.go
+++ main.go
@@ -64,6 +64,7 @@ func main() {
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUserUpdate)
 
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
